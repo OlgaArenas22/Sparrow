69d33d9ace015b3c018c5c17b73c1e71070f62d6
OlgaArenas22 <olga.arenas.rosario@alumnos.upm.es>
2025-11-03 22:14:34 +0100

    first commit

---
+++ app/src/main/java/es/upm/miw/sparrow/AuthActivity.java
@@ -0,0 +1,26 @@
+package es.upm.miw.sparrow;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import es.upm.miw.sparrow.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_auth);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.email), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
